780f15309120c70df43ba9cd3d875d65b1b5129f
gouguoyin <245629560@qq.com>
2025-03-27 17:33:31 +0800

    test: Add test cases for conversion between layout and format

--- parser_unit_test.go
+++ parser_unit_test.go
@@ -106,7 +106,9 @@ func TestParseByFormat(t *testing.T) {
 
 	t.Run("valid time without timezone", func(t *testing.T) {
 		assert.Equal(t, "2020-08-05 00:00:00 +0000 UTC", ParseByFormat("2020-08-05", DateFormat).ToString())
+		assert.Equal(t, "0000-01-01 13:14:15 +0000 UTC", ParseByFormat("13:14:15", TimeFormat).ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0000 UTC", ParseByFormat("2020-08-05 13:14:15", DateTimeFormat).ToString())
+
 		assert.Equal(t, "2020-08-05 13:14:15 +0000 UTC", ParseByFormat("2020|08|05 13:14:15", "Y|m|d H:i:s").ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0000 UTC", ParseByFormat("It is 2020-08-05 13:14:15", "\\I\\t \\i\\s Y-m-d H:i:s").ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0000 UTC", ParseByFormat("今天是 2020年08月05日13时14分15秒", "今天是 Y年m月d日H时i分s秒").ToString())
@@ -114,7 +116,9 @@ func TestParseByFormat(t *testing.T) {
 
 	t.Run("valid time with timezone", func(t *testing.T) {
 		assert.Equal(t, "2020-08-05 00:00:00 +0800 CST", ParseByFormat("2020-08-05", DateFormat, PRC).ToString())
+		assert.Equal(t, "0000-01-01 13:14:15 +0805 LMT", ParseByFormat("13:14:15", TimeFormat, PRC).ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0800 CST", ParseByFormat("2020-08-05 13:14:15", DateTimeFormat, PRC).ToString())
+
 		assert.Equal(t, "2020-08-05 13:14:15 +0800 CST", ParseByFormat("2020|08|05 13:14:15", "Y|m|d H:i:s", PRC).ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0800 CST", ParseByFormat("It is 2020-08-05 13:14:15", "\\I\\t \\i\\s Y-m-d H:i:s", PRC).ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0800 CST", ParseByFormat("今天是 2020年08月05日13时14分15秒", "今天是 Y年m月d日H时i分s秒", PRC).ToString())
@@ -167,14 +171,20 @@ func TestParseByLayout(t *testing.T) {
 
 	t.Run("valid time without timezone", func(t *testing.T) {
 		assert.Equal(t, "2020-08-05 00:00:00 +0000 UTC", ParseByLayout("2020-08-05", DateLayout).ToString())
+		assert.Equal(t, "0000-01-01 13:14:15 +0000 UTC", ParseByLayout("13:14:15", TimeLayout).ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0000 UTC", ParseByLayout("2020-08-05 13:14:15", DateTimeLayout).ToString())
+
 		assert.Equal(t, "2020-08-05 13:14:15 +0000 UTC", ParseByLayout("2020|08|05 13:14:15", "2006|01|02 15:04:05").ToString())
+		assert.Equal(t, "2020-08-05 13:14:15 +0000 UTC", ParseByFormat("It is 2020-08-05 13:14:15", "\\I\\t \\i\\s 2006-01-02 15:04:05").ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0000 UTC", ParseByLayout("今天是 2020年08月05日13时14分15秒", "今天是 2006年01月02日15时04分05秒").ToString())
 	})
 
 	t.Run("valid time with timezone", func(t *testing.T) {
 		assert.Equal(t, "2020-08-05 00:00:00 +0800 CST", ParseByLayout("2020-08-05", DateLayout, PRC).ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0800 CST", ParseByLayout("2020-08-05 13:14:15", DateTimeLayout, PRC).ToString())
+		assert.Equal(t, "2020-08-05 13:14:15 +0800 CST", ParseByLayout("2020-08-05 13:14:15", DateTimeLayout, PRC).ToString())
+
+		assert.Equal(t, "2020-08-05 13:14:15 +0800 CST", ParseByFormat("It is 2020-08-05 13:14:15", "\\I\\t \\i\\s 2006-01-02 15:04:05", PRC).ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0800 CST", ParseByLayout("2020|08|05 13:14:15", "2006|01|02 15:04:05", PRC).ToString())
 		assert.Equal(t, "2020-08-05 13:14:15 +0800 CST", ParseByLayout("今天是 2020年08月05日13时14分15秒", "今天是 2006年01月02日15时04分05秒", PRC).ToString())
 	})
@@ -251,5 +261,137 @@ func TestParseWithFormats(t *testing.T) {
 		c := ParseWithFormats("2020|08|05 13|14|15", []string{"Y|m|d H|i|s", "y|m|d h|i|s"}, PRC)
 		assert.Equal(t, "2020-08-05 13:14:15 +0800 CST", c.ToString())
 		assert.Equal(t, "2006|01|02 15|04|05", c.CurrentLayout())
+	})
+}
+
+func TestFormat2Layout(t *testing.T) {
+	c := Parse("2020-08-05 13:14:15.999999999")
+
+	t.Run("without timezone", func(t *testing.T) {
+		assert.Equal(t, c.Layout(AtomLayout), c.Format(AtomFormat))
+		assert.Equal(t, c.Layout(ANSICLayout), c.Format(ANSICFormat))
+		assert.Equal(t, c.Layout(CookieLayout), c.Format(CookieFormat))
+		assert.Equal(t, c.Layout(KitchenLayout), c.Format(KitchenFormat))
+		assert.Equal(t, c.Layout(RssLayout), c.Format(RssFormat))
+		assert.Equal(t, c.Layout(RubyDateLayout), c.Format(RubyDateFormat))
+		assert.Equal(t, c.Layout(UnixDateLayout), c.Format(UnixDateFormat))
+
+		assert.Equal(t, c.Layout(RFC1036Layout), c.Format(RFC1036Format))
+		assert.Equal(t, c.Layout(RFC1123Layout), c.Format(RFC1123Format))
+		assert.Equal(t, c.Layout(RFC1123ZLayout), c.Format(RFC1123ZFormat))
+		assert.Equal(t, c.Layout(RFC2822Layout), c.Format(RFC2822Format))
+		assert.Equal(t, c.Layout(RFC3339Layout), c.Format(RFC3339Format))
+		assert.Equal(t, c.Layout(RFC3339MilliLayout), c.Format(RFC3339MilliFormat))
+		assert.Equal(t, c.Layout(RFC3339MicroLayout), c.Format(RFC3339MicroFormat))
+		assert.Equal(t, c.Layout(RFC3339NanoLayout), c.Format(RFC3339NanoFormat))
+		assert.Equal(t, c.Layout(RFC7231Layout), c.Format(RFC7231Format))
+		assert.Equal(t, c.Layout(RFC822Layout), c.Format(RFC822Format))
+		assert.Equal(t, c.Layout(RFC822ZLayout), c.Format(RFC822ZFormat))
+		assert.Equal(t, c.Layout(RFC850Layout), c.Format(RFC850Format))
+
+		assert.Equal(t, c.Layout(ISO8601Layout), c.Format(ISO8601Format))
+		assert.Equal(t, c.Layout(ISO8601MilliLayout), c.Format(ISO8601MilliFormat))
+		assert.Equal(t, c.Layout(ISO8601MicroLayout), c.Format(ISO8601MicroFormat))
+		assert.Equal(t, c.Layout(ISO8601NanoLayout), c.Format(ISO8601NanoFormat))
+
+		assert.Equal(t, c.Layout(ISO8601ZuluLayout), c.Format(ISO8601ZuluFormat))
+		assert.Equal(t, c.Layout(ISO8601ZuluMilliLayout), c.Format(ISO8601ZuluMilliFormat))
+		assert.Equal(t, c.Layout(ISO8601ZuluMicroLayout), c.Format(ISO8601ZuluMicroFormat))
+		assert.Equal(t, c.Layout(ISO8601ZuluNanoLayout), c.Format(ISO8601ZuluNanoFormat))
+
+		assert.Equal(t, c.Layout(FormattedDateLayout), c.Format(FormattedDateFormat))
+		assert.Equal(t, c.Layout(FormattedDayDateLayout), c.Format(FormattedDayDateFormat))
+
+		assert.Equal(t, c.Layout(DayDateTimeLayout), c.Format(DayDateTimeFormat))
+		assert.Equal(t, c.Layout(DateTimeLayout), c.Format(DateTimeFormat))
+		assert.Equal(t, c.Layout(DateTimeMilliLayout), c.Format(DateTimeMilliFormat))
+		assert.Equal(t, c.Layout(DateTimeMicroLayout), c.Format(DateTimeMicroFormat))
+		assert.Equal(t, c.Layout(DateTimeNanoLayout), c.Format(DateTimeNanoFormat))
+		assert.Equal(t, c.Layout(ShortDateTimeLayout), c.Format(ShortDateTimeFormat))
+		assert.Equal(t, c.Layout(ShortDateTimeMilliLayout), c.Format(ShortDateTimeMilliFormat))
+		assert.Equal(t, c.Layout(ShortDateTimeMicroLayout), c.Format(ShortDateTimeMicroFormat))
+		assert.Equal(t, c.Layout(ShortDateTimeNanoLayout), c.Format(ShortDateTimeNanoFormat))
+
+		assert.Equal(t, c.Layout(DateLayout), c.Format(DateFormat))
+		assert.Equal(t, c.Layout(DateMilliLayout), c.Format(DateMilliFormat))
+		assert.Equal(t, c.Layout(DateMicroLayout), c.Format(DateMicroFormat))
+		assert.Equal(t, c.Layout(DateNanoLayout), c.Format(DateNanoFormat))
+		assert.Equal(t, c.Layout(ShortDateLayout), c.Format(ShortDateFormat))
+		assert.Equal(t, c.Layout(ShortDateMilliLayout), c.Format(ShortDateMilliFormat))
+		assert.Equal(t, c.Layout(ShortDateMicroLayout), c.Format(ShortDateMicroFormat))
+		assert.Equal(t, c.Layout(ShortDateNanoLayout), c.Format(ShortDateNanoFormat))
+
+		assert.Equal(t, c.Layout(TimeLayout), c.Format(TimeFormat))
+		assert.Equal(t, c.Layout(TimeMilliLayout), c.Format(TimeMilliFormat))
+		assert.Equal(t, c.Layout(TimeMicroLayout), c.Format(TimeMicroFormat))
+		assert.Equal(t, c.Layout(TimeNanoLayout), c.Format(TimeNanoFormat))
+		assert.Equal(t, c.Layout(ShortTimeLayout), c.Format(ShortTimeFormat))
+		assert.Equal(t, c.Layout(ShortTimeMilliLayout), c.Format(ShortTimeMilliFormat))
+		assert.Equal(t, c.Layout(ShortTimeMicroLayout), c.Format(ShortTimeMicroFormat))
+		assert.Equal(t, c.Layout(ShortTimeNanoLayout), c.Format(ShortTimeNanoFormat))
+	})
+
+	t.Run("with timezone", func(t *testing.T) {
+		assert.Equal(t, c.Layout(AtomLayout, PRC), c.Format(AtomFormat, PRC))
+		assert.Equal(t, c.Layout(ANSICLayout, PRC), c.Format(ANSICFormat, PRC))
+		assert.Equal(t, c.Layout(CookieLayout, PRC), c.Format(CookieFormat, PRC))
+		assert.Equal(t, c.Layout(KitchenLayout, PRC), c.Format(KitchenFormat, PRC))
+		assert.Equal(t, c.Layout(RssLayout, PRC), c.Format(RssFormat, PRC))
+		assert.Equal(t, c.Layout(RubyDateLayout, PRC), c.Format(RubyDateFormat, PRC))
+		assert.Equal(t, c.Layout(UnixDateLayout, PRC), c.Format(UnixDateFormat, PRC))
+
+		assert.Equal(t, c.Layout(RFC1036Layout, PRC), c.Format(RFC1036Format, PRC))
+		assert.Equal(t, c.Layout(RFC1123Layout, PRC), c.Format(RFC1123Format, PRC))
+		assert.Equal(t, c.Layout(RFC1123ZLayout, PRC), c.Format(RFC1123ZFormat, PRC))
+		assert.Equal(t, c.Layout(RFC2822Layout, PRC), c.Format(RFC2822Format, PRC))
+		assert.Equal(t, c.Layout(RFC3339Layout, PRC), c.Format(RFC3339Format, PRC))
+		assert.Equal(t, c.Layout(RFC3339MilliLayout, PRC), c.Format(RFC3339MilliFormat, PRC))
+		assert.Equal(t, c.Layout(RFC3339MicroLayout, PRC), c.Format(RFC3339MicroFormat, PRC))
+		assert.Equal(t, c.Layout(RFC3339NanoLayout, PRC), c.Format(RFC3339NanoFormat, PRC))
+		assert.Equal(t, c.Layout(RFC7231Layout, PRC), c.Format(RFC7231Format, PRC))
+		assert.Equal(t, c.Layout(RFC822Layout, PRC), c.Format(RFC822Format, PRC))
+		assert.Equal(t, c.Layout(RFC822ZLayout, PRC), c.Format(RFC822ZFormat, PRC))
+		assert.Equal(t, c.Layout(RFC850Layout, PRC), c.Format(RFC850Format, PRC))
+
+		assert.Equal(t, c.Layout(ISO8601Layout, PRC), c.Format(ISO8601Format, PRC))
+		assert.Equal(t, c.Layout(ISO8601MilliLayout, PRC), c.Format(ISO8601MilliFormat))
+		assert.Equal(t, c.Layout(ISO8601MicroLayout, PRC), c.Format(ISO8601MicroFormat))
+		assert.Equal(t, c.Layout(ISO8601NanoLayout, PRC), c.Format(ISO8601NanoFormat))
+
+		assert.Equal(t, c.Layout(ISO8601ZuluLayout, PRC), c.Format(ISO8601ZuluFormat))
+		assert.Equal(t, c.Layout(ISO8601ZuluMilliLayout, PRC), c.Format(ISO8601ZuluMilliFormat))
+		assert.Equal(t, c.Layout(ISO8601ZuluMicroLayout, PRC), c.Format(ISO8601ZuluMicroFormat))
+		assert.Equal(t, c.Layout(ISO8601ZuluNanoLayout, PRC), c.Format(ISO8601ZuluNanoFormat))
+
+		assert.Equal(t, c.Layout(FormattedDateLayout, PRC), c.Format(FormattedDateFormat))
+		assert.Equal(t, c.Layout(FormattedDayDateLayout, PRC), c.Format(FormattedDayDateFormat))
+
+		assert.Equal(t, c.Layout(DayDateTimeLayout, PRC), c.Format(DayDateTimeFormat))
+		assert.Equal(t, c.Layout(DateTimeLayout, PRC), c.Format(DateTimeFormat))
+		assert.Equal(t, c.Layout(DateTimeMilliLayout, PRC), c.Format(DateTimeMilliFormat))
+		assert.Equal(t, c.Layout(DateTimeMicroLayout, PRC), c.Format(DateTimeMicroFormat))
+		assert.Equal(t, c.Layout(DateTimeNanoLayout, PRC), c.Format(DateTimeNanoFormat))
+		assert.Equal(t, c.Layout(ShortDateTimeLayout, PRC), c.Format(ShortDateTimeFormat))
+		assert.Equal(t, c.Layout(ShortDateTimeMilliLayout, PRC), c.Format(ShortDateTimeMilliFormat))
+		assert.Equal(t, c.Layout(ShortDateTimeMicroLayout, PRC), c.Format(ShortDateTimeMicroFormat))
+		assert.Equal(t, c.Layout(ShortDateTimeNanoLayout, PRC), c.Format(ShortDateTimeNanoFormat))
+
+		assert.Equal(t, c.Layout(DateLayout, PRC), c.Format(DateFormat))
+		assert.Equal(t, c.Layout(DateMilliLayout, PRC), c.Format(DateMilliFormat, PRC))
+		assert.Equal(t, c.Layout(DateMicroLayout, PRC), c.Format(DateMicroFormat, PRC))
+		assert.Equal(t, c.Layout(DateNanoLayout, PRC), c.Format(DateNanoFormat, PRC))
+		assert.Equal(t, c.Layout(ShortDateLayout, PRC), c.Format(ShortDateFormat, PRC))
+		assert.Equal(t, c.Layout(ShortDateMilliLayout, PRC), c.Format(ShortDateMilliFormat, PRC))
+		assert.Equal(t, c.Layout(ShortDateMicroLayout, PRC), c.Format(ShortDateMicroFormat, PRC))
+		assert.Equal(t, c.Layout(ShortDateNanoLayout, PRC), c.Format(ShortDateNanoFormat, PRC))
+
+		assert.Equal(t, c.Layout(TimeLayout, PRC), c.Format(TimeFormat), PRC)
+		assert.Equal(t, c.Layout(TimeMilliLayout, PRC), c.Format(TimeMilliFormat, PRC))
+		assert.Equal(t, c.Layout(TimeMicroLayout, PRC), c.Format(TimeMicroFormat, PRC))
+		assert.Equal(t, c.Layout(TimeNanoLayout, PRC), c.Format(TimeNanoFormat, PRC))
+		assert.Equal(t, c.Layout(ShortTimeLayout, PRC), c.Format(ShortTimeFormat, PRC))
+		assert.Equal(t, c.Layout(ShortTimeMilliLayout, PRC), c.Format(ShortTimeMilliFormat, PRC))
+		assert.Equal(t, c.Layout(ShortTimeMicroLayout, PRC), c.Format(ShortTimeMicroFormat, PRC))
+		assert.Equal(t, c.Layout(ShortTimeNanoLayout, PRC), c.Format(ShortTimeNanoFormat, PRC))
 	})
 }
