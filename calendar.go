d60025e3225fbfe44a98dd886d38122843d16942
gouguoyin <245629560@qq.com>
2025-03-30 11:14:27 +0800

    refactor: Refactoring calendar conversion related methods

--- calendar.go
+++ calendar.go
@@ -17,56 +17,58 @@ func (c *Carbon) Lunar() *lunar.Lunar {
 		l.Error = c.Error
 		return l
 	}
-	return lunar.FromGregorian(c.StdTime()).ToLunar()
+	return lunar.FromStdTime(c.StdTime())
 }
 
 // CreateFromLunar creates a Carbon instance from Lunar date and time.
 // 从 农历日期 创建 Carbon 实例
-func CreateFromLunar(year, month, day, hour, minute, second int, isLeapMonth bool) *Carbon {
-	l := lunar.FromLunar(year, month, day, hour, minute, second, isLeapMonth)
+func CreateFromLunar(year, month, day int, isLeapMonth bool) *Carbon {
+	l := lunar.NewLunar(year, month, day, isLeapMonth)
 	if !l.IsValid() {
 		return nil
 	}
-	t := l.ToGregorian().Time
-	return CreateFromStdTime(t)
+	return NewCarbon(l.ToGregorian().Time)
 }
 
 // Julian converts Carbon instance to Julian instance.
 // 将 Carbon 实例转化为 Julian 实例
 func (c *Carbon) Julian() *julian.Julian {
-	if c.IsInvalid() {
+	j := new(julian.Julian)
+	if c.IsNil() {
 		return nil
 	}
-	return julian.FromGregorian(c.StdTime()).ToJulian()
+	if c.HasError() {
+		return j
+	}
+	return julian.FromStdTime(c.StdTime())
 }
 
 // CreateFromJulian creates a Carbon instance from Julian Day or Modified Julian Day.
 // 从 儒略日/简化儒略日 创建 Carbon 实例
 func CreateFromJulian(f float64) *Carbon {
-	g := julian.FromJulian(f).ToGregorian()
-	if !g.IsValid() {
-		return nil
-	}
-	t := julian.FromJulian(f).ToGregorian().Time
-	return CreateFromStdTime(t)
+	j := julian.NewJulian(f)
+	return NewCarbon(j.ToGregorian().Time)
 }
 
 // Persian converts Carbon instance to Persian instance.
 // 将 Carbon 实例转化为 Persian 实例
 func (c *Carbon) Persian() *persian.Persian {
 	p := new(persian.Persian)
-	if c.IsInvalid() {
+	if c.IsNil() {
+		return nil
+	}
+	if c.HasError() {
 		return p
 	}
-	return persian.FromGregorian(c.StdTime()).ToPersian()
+	return persian.FromStdTime(c.StdTime())
 }
 
 // CreateFromPersian creates a Carbon instance from Persian date and time.
 // 从 波斯日期 创建 Carbon 实例
-func CreateFromPersian(year, month, day, hour, minute, second int) *Carbon {
-	p := persian.FromPersian(year, month, day, hour, minute, second)
-	if p == nil || p.Error != nil {
+func CreateFromPersian(year, month, day int) *Carbon {
+	p := persian.NewPersian(year, month, day)
+	if !p.IsValid() {
 		return nil
 	}
-	return CreateFromStdTime(p.ToGregorian().Time)
+	return NewCarbon(p.ToGregorian().Time)
 }
