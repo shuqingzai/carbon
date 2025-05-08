082239206a76212ca62fd902c1ebeec927e44b47
kuafuRace <245629560@qq.com>
2025-05-09 01:15:38 +0800

    test: Add CURD test cases

--- tests/connect.go
+++ tests/connect.go
@@ -12,27 +12,31 @@ import (
 )
 
 var (
-	db *gorm.DB
+	dsn string
+	dia gorm.Dialector
+	db  *gorm.DB
 )
 
 func connect(driver string) *gorm.DB {
 	if err := godotenv.Load(".env"); err != nil {
-		panic("`.env` file does not exist, please copy `.env.example` file to `.env` file")
+		fmt.Printf("`.env` file does not exist, please copy `.env.example` file to `.env` file")
+		os.Exit(1)
 	}
-	var dia gorm.Dialector
+
 	switch driver {
 	case "mysql":
-		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("MySQL_DB_USERNAME"), os.Getenv("MySQL_DB_PASSWORD"), os.Getenv("MySQL_DB_HOST"), os.Getenv("MySQL_DB_PORT"), os.Getenv("MySQL_DB_DATABASE"))
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("MySQL_DB_USERNAME"), os.Getenv("MySQL_DB_PASSWORD"), os.Getenv("MySQL_DB_HOST"), os.Getenv("MySQL_DB_PORT"), os.Getenv("MySQL_DB_DATABASE"))
 		dia = mysql.Open(dsn)
 	case "pgsql":
-		dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable TimeZone=Asia/Shanghai", os.Getenv("PgSQL_DB_HOST"), os.Getenv("PgSQL_DB_PORT"), os.Getenv("PgSQL_DB_USERNAME"), os.Getenv("PgSQL_DB_DATABASE"), os.Getenv("PgSQL_DB_PASSWORD"))
+		dsn = fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable TimeZone=Asia/Shanghai", os.Getenv("PgSQL_DB_HOST"), os.Getenv("PgSQL_DB_PORT"), os.Getenv("PgSQL_DB_USERNAME"), os.Getenv("PgSQL_DB_DATABASE"), os.Getenv("PgSQL_DB_PASSWORD"))
 		dia = postgres.Open(dsn)
 	}
 	db, err := gorm.Open(dia, &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 	if err != nil {
-		panic("failed to connect database")
+		fmt.Printf("failed to connect database, dsn:%s", dsn)
+		os.Exit(1)
 	}
 	return db
 }
