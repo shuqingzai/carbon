2650bdc664caac7c0ad6bee4278e84d7f0275807
kuafuRace <245629560@qq.com>
2025-05-25 23:00:49 +0800

    refactor: Update comments

--- parser.go
+++ parser.go
@@ -72,21 +72,25 @@ func ParseByLayout(value, layout string, timezone ...string) *Carbon {
 
 	// timestamp layouts
 	switch layout {
+	// Deprecated: it will be removed in the future, use "CreateFromTimestamp" instead.
 	case TimestampLayout:
 		if ts, err = parseTimestamp(value); err != nil {
 			return &Carbon{Error: err}
 		}
 		return CreateFromTimestamp(ts).SetLocation(loc)
+	// Deprecated: it will be removed in the future, use "CreateFromTimestampMilli" instead.
 	case TimestampMilliLayout:
 		if ts, err = parseTimestamp(value); err != nil {
 			return &Carbon{Error: err}
 		}
 		return CreateFromTimestampMilli(ts).SetLocation(loc)
+	// Deprecated: it will be removed in the future, use "CreateFromTimestampMicro" instead.
 	case TimestampMicroLayout:
 		if ts, err = parseTimestamp(value); err != nil {
 			return &Carbon{Error: err}
 		}
 		return CreateFromTimestampMicro(ts).SetLocation(loc)
+	// Deprecated: it will be removed in the future, use "CreateFromTimestampNano" instead.
 	case TimestampNanoLayout:
 		if ts, err = parseTimestamp(value); err != nil {
 			return &Carbon{Error: err}
