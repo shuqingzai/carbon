3d6f3948b6e51282e59c82e18d4ae2adb15dcdd7
kuafuRace <245629560@qq.com>
2025-07-05 02:21:50 +0800

    feat: Add concurrent performance testing

--- boundary_bench_test.go
+++ boundary_bench_test.go
@@ -6,160 +6,500 @@ import (
 
 func BenchmarkCarbon_StartOfCentury(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.StartOfCentury()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.StartOfCentury()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.StartOfCentury()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_EndOfCentury(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.EndOfCentury()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.EndOfCentury()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.EndOfCentury()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_StartOfDecade(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.StartOfDecade()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.StartOfDecade()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.StartOfDecade()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_EndOfDecade(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.EndOfDecade()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.EndOfDecade()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.EndOfDecade()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_StartOfYear(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.StartOfYear()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.StartOfYear()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.StartOfYear()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_EndOfYear(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.EndOfYear()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.EndOfYear()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.EndOfYear()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_StartOfQuarter(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.StartOfQuarter()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.StartOfQuarter()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.StartOfQuarter()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_EndOfQuarter(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.EndOfQuarter()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.EndOfQuarter()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.EndOfQuarter()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_StartOfMonth(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.StartOfMonth()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.StartOfMonth()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.StartOfMonth()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_EndOfMonth(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.EndOfMonth()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.EndOfMonth()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.EndOfMonth()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_StartOfWeek(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.StartOfWeek()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.StartOfWeek()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.StartOfWeek()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_EndOfWeek(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.EndOfWeek()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.EndOfWeek()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.EndOfWeek()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_StartOfDay(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.StartOfDay()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.StartOfDay()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.StartOfDay()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_EndOfDay(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.EndOfDay()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.EndOfDay()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.EndOfDay()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_StartOfHour(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.StartOfHour()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.StartOfHour()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.StartOfHour()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_EndOfHour(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.EndOfHour()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.EndOfHour()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.EndOfHour()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_StartOfMinute(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.StartOfMinute()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.StartOfMinute()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.StartOfMinute()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_EndOfMinute(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.EndOfMinute()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.EndOfMinute()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.EndOfMinute()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_StartOfSecond(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.StartOfSecond()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.StartOfSecond()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.StartOfSecond()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_EndOfSecond(b *testing.B) {
 	c := Now()
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c.EndOfSecond()
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c.EndOfSecond()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c.EndOfSecond()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
--- extremum_bench_test.go
+++ extremum_bench_test.go
@@ -5,81 +5,245 @@ import (
 )
 
 func BenchmarkZeroValue(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		ZeroValue()
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			ZeroValue()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				ZeroValue()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkEpochValue(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		EpochValue()
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			EpochValue()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				EpochValue()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkMaxValue(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		MaxValue()
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			MaxValue()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				MaxValue()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkMinValue(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		MinValue()
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			MinValue()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				MinValue()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkMaxDuration(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		MaxDuration()
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			MaxDuration()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				MaxDuration()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkMinDuration(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		MinDuration()
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			MinDuration()
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				MinDuration()
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkMax(b *testing.B) {
 	c1 := Parse("2020-08-06")
 	c2 := Parse("2021-08-05")
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		Max(c1, c2)
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			Max(c1, c2)
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				Max(c1, c2)
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkMin(b *testing.B) {
 	c1 := Parse("2020-08-06")
 	c2 := Parse("2021-08-05")
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		Min(c1, c2)
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			Min(c1, c2)
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				Min(c1, c2)
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_Closest(b *testing.B) {
 	c1 := Parse("2020-08-04")
 	c2 := Parse("2020-08-05")
 	c3 := Parse("2021-08-06")
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c1.Closest(c2, c3)
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c1.Closest(c2, c3)
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c1.Closest(c2, c3)
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkCarbon_Farthest(b *testing.B) {
 	c1 := Parse("2020-08-04")
 	c2 := Parse("2020-08-05")
 	c3 := Parse("2021-08-06")
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		c1.Farthest(c2, c3)
-	}
+
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			c1.Farthest(c2, c3)
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				c1.Farthest(c2, c3)
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
