cf8d8e7fcd867007e0fc7fa61b319dce5d86da0d
kuafuRace <245629560@qq.com>
2025-07-05 01:43:00 +0800

    feat: Add concurrent performance testing

--- parser_bench_test.go
+++ parser_bench_test.go
@@ -5,36 +5,116 @@ import (
 )
 
 func BenchmarkParse(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		Parse("2020-08-05 01:02:03")
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			Parse("2020-08-05 01:02:03")
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				Parse("2020-08-05 01:02:03")
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkParseByLayout(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		ParseByLayout("2020-08-05 13:14:15", DateTimeLayout)
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			ParseByLayout("2020-08-05 13:14:15", DateTimeLayout)
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				ParseByLayout("2020-08-05 13:14:15", DateTimeLayout)
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkParseByFormat(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		ParseByFormat("2020-08-05 13:14:15", DateTimeFormat)
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			ParseByFormat("2020-08-05 13:14:15", DateTimeFormat)
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				ParseByFormat("2020-08-05 13:14:15", DateTimeFormat)
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkParseByLayouts(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		ParseByLayouts("2020-08-05 13:14:15", []string{DateLayout, DateTimeLayout})
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			ParseByLayouts("2020-08-05 13:14:15", []string{DateLayout, DateTimeLayout})
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				ParseByLayouts("2020-08-05 13:14:15", []string{DateLayout, DateTimeLayout})
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
 
 func BenchmarkParseByFormats(b *testing.B) {
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		ParseByFormats("2020-08-05 13:14:15", []string{DateFormat, DateTimeFormat})
-	}
+	b.Run("sequential", func(b *testing.B) {
+		b.ResetTimer()
+		for n := 0; n < b.N; n++ {
+			ParseByFormats("2020-08-05 13:14:15", []string{DateFormat, DateTimeFormat})
+		}
+	})
+
+	b.Run("concurrent", func(b *testing.B) {
+		done := make(chan bool, b.N)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				ParseByFormats("2020-08-05 13:14:15", []string{DateFormat, DateTimeFormat})
+				done <- true
+			}()
+		}
+		for i := 0; i < b.N; i++ {
+			<-done
+		}
+	})
 }
