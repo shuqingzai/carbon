7b9a38ec2ad72091a545735896259ad2f1df7d2b
kuafuRace <245629560@qq.com>
2025-07-07 08:34:18 +0800

    Fix: Fix concurrency issues with the `Sleep` function in the testing environment

--- carbon.go
+++ carbon.go
@@ -65,7 +65,9 @@ func (c *Carbon) Copy() *Carbon {
 // Sleep sleeps for the specified duration like time.Sleep.
 func (c *Carbon) Sleep(d time.Duration) {
 	if IsTestNow() && d > 0 {
+		frozenNow.rw.Lock()
 		frozenNow.testNow = frozenNow.testNow.AddDuration(d.String())
+		frozenNow.rw.Unlock()
 		return
 	}
 	time.Sleep(d)
